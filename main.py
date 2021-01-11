9fcf43a19750a1fc2d341d13824ba426d58fcb38
dgarmsiri <77160290+dgarmsiri@users.noreply.github.com>
2021-01-11 09:54:30 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ def on_forever():
     . # . # .
     . . # . .
     """)
-for i in range(4):
+for i in range(2):
     basic.show_icon(IconNames.HEART)
     basic.forever(on_forever)
+    
+basic.clear_screen()
+
+def on_button_pressed_a():
+    radio.send_string("HI")    
+input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_received_string(receivedString):
-    pass
+    basic.show_string(receivedString)
 radio.on_received_string(on_received_string)
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < 2; i++) {
     basic.showIcon(IconNames.Heart)
     basic.forever(function on_forever() {
         basic.showLeds(`
@@ -10,6 +10,10 @@ for (let i = 0; i < 4; i++) {
     `)
     })
 }
+basic.clearScreen()
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    radio.sendString("HI")
+})
 radio.onReceivedString(function on_received_string(receivedString: string) {
-    
+    basic.showString(receivedString)
 })
